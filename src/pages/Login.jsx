f38847eff8e31d0ee568fecbcb68fd80911bcde8
Iroro1 <ojigboleo@gmail.com>
2022-09-20 14:37:01 +0100

    added bg

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,82 +1,91 @@
 import React from "react";
-import {Link} from 'react-router-dom'
+import { Link } from "react-router-dom";
 
 const Login = () => {
   return (
-    <div
-      style={{
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
-        height: "100vh",
-        width: "100%",
-      }}
-    >
+    <div className="bgAuth">
       <div
+        className="loginBg"
         style={{
           display: "flex",
-          flexDirection: "column",
+          justifyContent: "center",
+          alignItems: "center",
+          height: "100vh",
+          width: "100%",
         }}
       >
-        <div style={{ marginBottom: "15px" }}>
-          <h1 style={{ marginBottom: "2px" }}>DSF</h1>
-          <small>Consultants Portal</small>
-        </div>
         <div
           style={{
-            minHeight: "300px",
-            minWidth: "300px",
-            background: "#fff",
-            boxShadow: "3px 3px 4px 4px #ddd",
-            borderRadius: "8px",
-            padding: "10px",
-            position:"relative"
-
+            display: "flex",
+            flexDirection: "column",
           }}
         >
-          <h3
+          <div
             style={{
-              textAlign: "center",
-              fontWeight: "900",
-              marginBottom: "20px",
-              borderBottom: "1px solid #eee",
-              paddingBottom: "10px",
+              minHeight: "300px",
+              minWidth: "300px",
+              background: "#fff",
+              boxShadow: "3px 3px 4px 4px #ddd",
+              borderRadius: "8px",
+              padding: "10px",
+              position: "relative",
             }}
           >
-            Login
-          </h3>
-
-          <form className="mx-10">
-            <div className="form-group-col  mb15">
-              <label className="form-label" htmlFor="email">
-                Email
-              </label>
-              <input
-                className="form-input"
-                type="text"
-                name="email"
-                placeholder="jd@gmail.com"
-              />
+            <div style={{ marginBottom: "15px" }}>
+              <h1 style={{ marginBottom: "2px", color: "red" }}>DSF</h1>
+              <small>Consultants Portal</small>
             </div>
-            <div className="form-group-col mt15">
-              <label className="form-label" htmlFor="password">
-                Password
-              </label>
-              <input className="form-input" type="password" name="password" />
-            </div>
-
-            <button
-              className="btn btnOutlin btnSuccess textWhite mt15"
-              type="submit"
+            <h3
+              style={{
+                textAlign: "center",
+                fontWeight: "900",
+                marginBottom: "20px",
+                borderBottom: "1px solid #eee",
+                paddingBottom: "10px",
+                color: "red",
+              }}
             >
               Login
-            </button>
-          </form>
-          <Link style={{
-            position:"absolute",
-            bottom:"10px",
-            right:"10px"
-          }} to={"/register"} >Become a consultant.</Link>
+            </h3>
+
+            <form className="mx-10">
+              <div className="form-group-col  mb15">
+                <label className="form-label" htmlFor="email">
+                  Email
+                </label>
+                <input
+                  className="form-input"
+                  type="text"
+                  name="email"
+                  placeholder="jd@gmail.com"
+                />
+              </div>
+              <div className="form-group-col mt15">
+                <label className="form-label" htmlFor="password">
+                  Password
+                </label>
+                <input className="form-input" type="password" name="password" />
+              </div>
+
+              <button
+                className="btn btnOutlin btnSuccess textWhite mt15"
+                type="submit"
+              >
+                Login
+              </button>
+            </form>
+            <Link
+              style={{
+                position: "absolute",
+                bottom: "10px",
+                right: "10px",
+                fontSize: "10px",
+              }}
+              to={"/register"}
+            >
+              Become a consultant.
+            </Link>
+          </div>
         </div>
       </div>
     </div>
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,108 +1,116 @@
-import React from 'react'
-import {Link} from 'react-router-dom'
+import React from "react";
+import { Link } from "react-router-dom";
 
 const Register = () => {
   return (
-    <div
-    style={{
-      display: "flex",
-      justifyContent: "center",
-      alignItems: "center",
-      height: "100vh",
-      width: "100%",
-    }}
-  >
-    <div
-      style={{
-        display: "flex",
-        flexDirection: "column",
-      }}
-    >
-      <div style={{ marginBottom: "15px" }}>
-        <h1 style={{ marginBottom: "2px" }}>DSF</h1>
-        <small>Consultants Portal</small>
-      </div>
+    <div className="bgAuth">
       <div
         style={{
-          minHeight: "300px",
-          minWidth: "300px",
-          background: "#fff",
-          boxShadow: "3px 3px 4px 4px #ddd",
-          borderRadius: "8px",
-          padding: "10px",
-          position:"relative"
-
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          height: "100vh",
+          width: "100%",
         }}
+        className="loginBg"
       >
-        <h3
+        <div
           style={{
-            textAlign: "center",
-            fontWeight: "900",
-            marginBottom: "20px",
-            borderBottom: "1px solid #eee",
-            paddingBottom: "10px",
+            display: "flex",
+            flexDirection: "column",
           }}
         >
-          Register
-        </h3>
-
-        <form className="mx-10">
-        <div className="form-group-col  mb15">
-            <label className="form-label" htmlFor="email">
-              Firstname
-            </label>
-            <input
-              className="form-input"
-              type="text"
-              name="firstName"
-              placeholder="John"
-            />
-          </div>
-          <div className="form-group-col  mb15">
-            <label className="form-label" htmlFor="email">
-              Lastname
-            </label>
-            <input
-              className="form-input"
-              type="text"
-              name="lastname"
-              placeholder="Doe"
-            />
-          </div>
-          <div className="form-group-col  mb15">
-            <label className="form-label" htmlFor="email">
-              Email
-            </label>
-            <input
-              className="form-input"
-              type="text"
-              name="email"
-              placeholder="jd@gmail.com"
-            />
-          </div>
-          <div className="form-group-col mt15">
-            <label className="form-label" htmlFor="password">
-              Password
-            </label>
-            <input className="form-input" type="password" name="password" />
-          </div>
-
-          <button
-            className="btn btnOutlin btnPrimary textWhite mt15"
-            type="submit"
+          <div
+            style={{
+              minHeight: "300px",
+              minWidth: "300px",
+              background: "#fff",
+              boxShadow: "3px 3px 4px 4px #ddd",
+              borderRadius: "8px",
+              padding: "10px",
+              position: "relative",
+            }}
           >
-            Register
-          </button>
-        </form>
-        <Link style={{
-          position:"absolute",
-          bottom:"10px",
-          right:"10px"
-        }} to={"/register"} >Become a consultant.</Link>
+            <div style={{ marginBottom: "15px" }}>
+              <h1 style={{ marginBottom: "2px" }}>DSF</h1>
+              <small>Consultants Portal</small>
+            </div>
+            <h3
+              style={{
+                textAlign: "center",
+                fontWeight: "900",
+                marginBottom: "20px",
+                borderBottom: "1px solid #eee",
+                paddingBottom: "10px",
+              }}
+            >
+              Register
+            </h3>
+
+            <form className="mx-10">
+              <div className="form-group-col  mb15">
+                <label className="form-label" htmlFor="email">
+                  Firstname
+                </label>
+                <input
+                  className="form-input"
+                  type="text"
+                  name="firstName"
+                  placeholder="John"
+                />
+              </div>
+              <div className="form-group-col  mb15">
+                <label className="form-label" htmlFor="email">
+                  Lastname
+                </label>
+                <input
+                  className="form-input"
+                  type="text"
+                  name="lastname"
+                  placeholder="Doe"
+                />
+              </div>
+              <div className="form-group-col  mb15">
+                <label className="form-label" htmlFor="email">
+                  Email
+                </label>
+                <input
+                  className="form-input"
+                  type="text"
+                  name="email"
+                  placeholder="jd@gmail.com"
+                />
+              </div>
+              <div className="form-group-col mt15">
+                <label className="form-label" htmlFor="password">
+                  Password
+                </label>
+                <input className="form-input" type="password" name="password" />
+              </div>
+
+              <button
+                className="btn btnOutlin btnPrimary textWhite mt15"
+                type="submit"
+              >
+                Register
+              </button>
+            </form>
+            <Link
+              style={{
+                position: "absolute",
+                bottom: "10px",
+                right: "10px",
+                fontSize: "10px",
+              }}
+              to={"/"}
+            >
+              Login
+            </Link>
+          </div>
+        </div>
       </div>
     </div>
-  </div>
-  )
-}
+  );
+};
 
-export default Register
+export default Register;
